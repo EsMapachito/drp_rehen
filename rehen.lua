616139fae3a47c992c88a89775cf9e8316b67a66
Mapachito <58920957+EsMapachito@users.noreply.github.com>
2020-08-05 15:02:24 +0200

    Add files via upload

--- rehen.lua
+++ rehen.lua
@@ -3,23 +3,23 @@
 
 local keys = { ['G'] = 0x760A9C6F, ['S'] = 0xD27782E3, ['W'] = 0x8FD015D8, ['H'] = 0x24978A28, ['G'] = 0x5415BE48, ["ENTER"] = 0xC7B5340A, ['E'] = 0xDFF812F9,["BACKSPACE"] = 0x156F7119 }
 
---RegisterCommand("rio", function(source, args, rawCommand) -- slash COMMAND
+--RegisterCommand("miedodrp", function(source, args, rawCommand) -- slash COMMAND
 RegisterCommand("rehen", function(source, args)
 
-            if rio ~= 0 then
-                SetEntityAsMissionEntity(rio)
-                DeleteObject(nativerioprop)
-                rio = 0
+            if miedodrp ~= 0 then
+                SetEntityAsMissionEntity(miedodrp)
+                DeleteObject(nativemiedodrpprop)
+                miedodrp = 0
                 end
                 local playerPed = PlayerPedId()
                 Citizen.Wait(0)
                 ClearPedTasksImmediately(PlayerPedId())
-                TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_FLEE'), -1, true, false, false, false)
+                TaskStartScenamiedodrpInPlace(playerPed, GetHashKey('WORLD_HUMAN_FLEE'), -1, true, false, false, false)
                 local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, -1.55))
                 --local prop = CreateObject(GetHashKey("p_shavingbox01x"), x, y, z, true, false, true)
                 SetEntityHeading(prop, GetEntityHeading(PlayerPedId()))
                 PlaceObjectOnGroundProperly(prop)
-                rio = prop
+                miedodrp = prop
     
     --end, false)
 end)
@@ -30,11 +30,11 @@ Citizen.CreateThread(function()
         Citizen.Wait(0)
 		
         if whenKeyJustPressed(keys['BACKSPACE']) then --TABULADOR PARA DETENER ACCIÓN
-            if rio ~= 0 then
-            SetEntityAsMissionEntity(rio)
-            DeleteObject(rio)
+            if miedodrp ~= 0 then
+            SetEntityAsMissionEntity(miedodrp)
+            DeleteObject(miedodrp)
 			ClearPedTasksImmediately(PlayerPedId())
-            rio = 0
+            miedodrp = 0
             end
      end
 	end
